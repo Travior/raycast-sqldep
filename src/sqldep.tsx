320f044951e953060c618d1609d61c4505165d2a
Travior <30752199+Travior@users.noreply.github.com>
2023-04-14 15:06:04 +0200

    WIP column level lineage
    Needs refactoring

--- src/sqldep.tsx
+++ src/sqldep.tsx
@@ -54,7 +54,13 @@ function readFile(path: string) {
 }
 
 function sendRequest(content: string) {
+    console.log("Normal deps")
     return axios.get("http://127.0.0.1:3141/sqldep", { data: { sql: content }, responseType: "json" })
+}
+
+function sendColumnRequest(content: string){
+    console.log("Column Level Request")
+    return axios.get("http://127.0.0.1:3141/sqlcoldep", { data: { sql: content }, responseType: "json" })
 }
 
 export default function SQLDEP() {
@@ -63,10 +69,11 @@ export default function SQLDEP() {
     const [pathContent, setPathContent] = useState<Array<PathContentItem>>([]);
     const [transistionService, setTransitionService] = useState(null);
     const [response, setResponse] = useState(null);
-    const [statement, setStatement] = useState(null)
+    const [statement, setStatement] = useState(null);
+    const [column, setColumn] = useState(null);
 
     const statemachine = createMachine({
-        /** @xstate-layout N4IgpgJg5mDOIC5SwC4EMVgHQBsD2aEAlgHZQAKATmAGZjUkDGcAxACIDyAcgKIDaABgC6iUAAc8sIiiJ4SokAA9EAJgDMAdixqArAE41BgXoGaAHGpUAaEAE9EZrXoCMANlcbnKja+d6fKgC+gTaoGNj4hKQU1HQMzLAsXBwAKgD6AGIcAKpcbIIiSCASUjJyCsoI6lq6BkYm5pY29gjOACyuWG1urhZmKip6HWptwaHomFjE1IwoeJS2AGpEYADuLADKPHlpAEo8AIrZPBspBQol0rLyRZU6bWbaGjoCzmbOAjqGOmbNDk49TzeXz+VxBEIgMKTVZoaQZea7MAARwArnAUCx9kcTulOLw0gBZbIAGRSAEk0qcAIIpHgE7ZnYQXSRXcq3Bx6TqcxxmATvQzONTOP4Id5dPQSvS8wyWfp6MaQibYGFwhHItGoTGHY6nNJ4niUslcADixIN1Np9K4jMK4hZZRuoEqZk5WG5Gl5-JcQpFKgEKiwKjakueQz5Oh8Cqh2GjAFswCQUBswDgwLNrpseGaAMI25mla4VRDtNRqLBmDx+sFmB4aNQisXByXSgwqOXBCEkPAQOAKaP51mOpSIAC0rhFY6wAmn07cNenPlcowh0dwBGIZCotHoCYSA4dRYQbWsdkQQ3LpZGAzrOm8EqjSqmRBmcwWyzW+8L7IQhgD4bDrilkGAgaG0DaPN07gaBoEoPO0bQ6A+4RYCqKDwpQiKouin5sk6iAaG25ZSm0oEjO8rg6OOp6ihBPQ6M4XimCRzhIZMcYJkmKZpgeRSXDxeGtNUWBLgIYIev0bYeg2f4zgIJERkMLxmKx2A9mI76rDhQ6VM4Oj3E8LyWHJErtA2eiBk2NYIWovRyWoKlYGIsQ8JQlDzFph6-lOZj6G0olAX5oEiu0XI9IBUHGBGiEdkAA */
+        /** @xstate-layout N4IgpgJg5mDOIC5SwC4EMVgHQBsD2aEAlgHZQAKATmAGZjUkDGcAxACIDyAcgKIDaABgC6iUAAc8sIiiJ4SokAA9EAJgDMAdixqArAE5NAFgCMGgBwA2Y9bMAaEAE9Eai2qxmNOgRfU6b+tQBfQPtUDGx8QlIKajoGZlgWLg4AFQB9ADEOAFUuNkERJBAJKRk5BWUEdS1dAw0TcysbeycENQEdbU8BAXM1YwEVIeDQ9EwsYmpGFDxKBwA1IjAAdxYAZR48tIAlHgBFbJ41lIKFEulZeSLKnUMzLq9rFTNDATM9CxbnV3dun11-AYRiAwuNlmhpBlZtswABHACucBQLF2ByO6U4vDSAFlsgAZFIASTSxwAgikeNjNidhGdJBdytdEHpvFgPh5brcLBZDOYvlVDBZtGoRToLAJjCpjB8XMDQdhwZDoXDEagUftDsc0pieCTCVwAOJ43VkilUrg0wriellK6gSosoXszyGLk8vmORBmYxYLw9CXcvReHSaOVjbDygC2YBIKDWYBwYGml3WPGNAGFLXTSpcKohjHcVFhjDyBEGLHpjH4zCp+SpBlgVH7eo8zGY1HpDGHwlgozG4wmk7bUxn0umOHjTkVzra8wgCzXi6Xy5Xq7XPVUdEXugI1Cpua4212QiDw1hFSgoZQYQikSxx3idhr0druCb9UaTSlyZTqVPrTmjL2syrLOpygrunYG6aG4zZNiGIYeBY3bjIweA4PCkYkPGibTCOPCZv+xQ2rmTLzhoejbmohjUT4JjtpK-IuEKTY9M87SDCGgrBCeJB4BAcAKPK2YMnaSiIAAtJ8G4SZ0ejyXoGjmDY+46LcKERAQxBkFQtD0DGCQibOZGGOurS6FoQbtl4R56JYNEaRMRBTDMcyLCsRmkcBbSUVgbz6LyZYmPWUHmTyDwSp2QZKZRGiOReV43qqKCeUB4kIBozzuHZNEsu2ErPEx4WaI8UU6DFKhxSe8q9uG0axjhQ5ecRgFiZUkpmHJJX1gYxhmL0Gh1oK7giro4qStKrjIdVZ4CWI7nLKlbX5q4ei+lW3IlhRYoGPy0qdM2krtBorgFo5YixDwlCULMS1zh2Rb+Z2GhBZKbxMR0vx+n0AxDCojloTgbBgPNSyLdOJFpe1lZuCopmmJKW7qGoRVCju0oqEGal7gD6GYdhg7THdZElh261ilYFjbRWKPQdyWD1Ap+gaJoLPHsEQA */
         id: 'state',
         initial: 'loadingPreferences',
         predictableActionArguments: true,
@@ -77,25 +84,36 @@ export default function SQLDEP() {
                     NOT_FOUND: "prefError"
                 }
             },
+
             directoryView: {
                 on: {
                     SEND_REQUEST: "waitForRequest"
                 }
             },
+
             waitForRequest: {
                 on: {
-                    REQUEST_DONE_MULTI_STATEMENT: "statementSelection",
-                    REQUEST_DONE_SINGLE_STATEMENT: "depView"
+                    REQUEST_DONE_SINGLE_STATEMENT: "depView",
+                    COL_REQUEST_DONE_SINGLE_STATEMENT: "columnSelect",
+                    REQUEST_DONE_MULTI_STATEMENT:"statementSelection"
                 }
             },
+
             statementSelection: {
                 on: {
-                    SELECT: "depView"
+                    SELECT: "depView",
+                    SELECT_COL: "columnSelect"
                 }
             },
-            depView: {},
-            prefError: {}
 
+            depView: {},
+            prefError: {},
+            colDepView: {},
+            columnSelect: {
+                on:{
+                    COL_SELECT: "colDepView"
+                }
+            }
         }
     });
     useEffect(() => {
@@ -122,7 +140,7 @@ export default function SQLDEP() {
 
     return (
         <>
-            {state === "loadingPreferences" ?
+            {state === "loadingPreferences" || transistionService === null ?
                 <Detail isLoading={true} markdown="# Loading preferences" /> : null
             }
             {state === "prefError" ?
@@ -140,7 +158,7 @@ export default function SQLDEP() {
                                             setPaths([...paths, paths.at(-1) + element.name + "/"])
                                             setPathContent(content)
                                         })
-                                    }} /> : <Action title="Open File" onAction={() => {
+                                    }} /> : <><Action title="Open File" onAction={() => {
                                         readFile(paths.at(-1) + "/" + element.name).then((content) => {
                                             transistionService.send("SEND_REQUEST")
                                             sendRequest(content).then((value) => {
@@ -148,11 +166,27 @@ export default function SQLDEP() {
                                                 if (value.data.number_of_statements > 1) {
                                                     transistionService.send("REQUEST_DONE_MULTI_STATEMENT")
                                                 } else {
+                                                    setStatement(value.data.dependencies[0])
                                                     transistionService.send("REQUEST_DONE_SINGLE_STATEMENT")
                                                 }
                                             })
                                         })
-                                    }} />}
+                                    }} />
+                                    <Action title="Open File with column level lineage" onAction={() => {
+                                        readFile(paths.at(-1) + "/" + element.name).then((content) => {
+                                            transistionService.send("SEND_REQUEST")
+                                            sendColumnRequest(content).then((value) => {
+                                                setResponse(value.data);
+                                                if (value.data.number_of_statements > 1) {
+                                                    transistionService.send("REQUEST_DONE_MULTI_STATEMENT")
+                                                } else {
+                                                    setStatement(value.data.dependencies[0])
+                                                    transistionService.send("COL_REQUEST_DONE_SINGLE_STATEMENT")
+                                                }
+                                            })
+                                        }).catch((reason) => console.log(reason))
+                                    }}/></>
+                                    }
 
                                 </ActionPanel>
                             } />
@@ -163,27 +197,50 @@ export default function SQLDEP() {
             {state === "waitForRequest" ?
                 <Detail isLoading={true} markdown="# Loading dependencies" /> : null
             }
-            {state === "statementSelection" && response !== null ?
+            {state === "statementSelection" && response !== null && transistionService !== null ?
                 <List>
                     {response.dependencies.map((el: any, i: number) => {
                         return <List.Item key={"statement_" + i} title={el.statement} actions={
                             <ActionPanel>
                                 <Action title="Select statement" onAction={() => {
                                     setStatement(el);
-                                    transistionService.send("SELECT");
+                                    console.log(el)
+                                    if(el.columns!==undefined){
+                                        transistionService.send("SELECT_COL");
+                                    }
+                                    else{
+                                        transistionService.send("SELECT");
+                                    }
                                 }} />
                             </ActionPanel>
                         } />
                     })}
                 </List> : null
             }
-            {state === "depView" && statement!==null?
-            <List navigationTitle="Dependencies">
-                {statement.tables.map((table:string, i:number) => {
-                    return <List.Item key={"dependency_"+i} title={table} />
-                })}
+            {state === "depView" && statement !== null ?
+                <List searchBarPlaceholder={"Dependencies for table "+statement.target}>
+                    {statement.tables.map((table: string, i: number) => {
+                        return <List.Item key={"dependency_" + i} title={table} />
+                    })}
+                </List> : null
+            }
+            {state === "columnSelect" && statement !== null && statement.columns !== undefined?
+            <List searchBarPlaceholder="Select column you want to see dependencies for">
+                {statement.columns.map((col,i) => <List.Item key={"col_"+i} title={col.column} actions={
+                    <ActionPanel>
+                        <Action title="Show sources for column" onAction={() => {
+                            setColumn(col);
+                            transistionService.send("COL_SELECT");
+                        }}/>
+                    </ActionPanel>
+                }/>)}
             </List>:null
-        }
+            }
+            {state === "colDepView" && column !== null?
+                <List searchBarPlaceholder={"Dependencies for column "+column.column}>
+                    {column.dependencies.map((dep, i) => <List.Item key={"col_dep_"+i} title={dep}/>)}
+                </List>:null
+            }
         </>
     );
 }
